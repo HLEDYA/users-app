798218e3546b4c23b7b86d7395200b3043d29cd3
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-06 23:09:40 +0300

    validation checks

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -6,7 +6,7 @@ import Button from "../UI/Button";
 const InputForm = (props) => {
   const [user, setUser] = useState({
     name: "",
-    age: 0,
+    age: "",
   });
 
   const nameInputHandler = (event) => {
@@ -31,8 +31,20 @@ const InputForm = (props) => {
 
   const newUserHandler = (event) => {
     event.preventDefault();
+    if (user.name.trim().length === 0 || user.age.trim().length === 0) {
+      return;
+    }
+
+    if (+user.age < 1) {
+      return;
+    }
+
     console.log(user);
     props.onNewUser(user);
+    setUser({
+      name: "",
+      age: "",
+    });
   };
 
   return (
